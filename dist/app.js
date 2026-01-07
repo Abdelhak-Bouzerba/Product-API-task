57b200dbd90e84fff91463c2e7d550075c9789e4
Abdelhak-Bouzerba <abdelhakbouzerba6@gmail.com>
2026-01-07 22:06:44 +0100

    fix bugs for deployement

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import productRouter from './routes/product.js';
 const app = express();
 //middlewares
 app.use(express.json());
 //routes
+app.use('/api', productRouter);
 export default app;
